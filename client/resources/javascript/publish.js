cd32e4d062136fc8b0b9f15ed15b26776fd6073d
Surya Jasper <suryajasper2004@gmail.com>
2020-06-03 23:37:15 -0700

    added preview

--- client/resources/javascript/publish.js
+++ client/resources/javascript/publish.js
@@ -1,5 +1,13 @@
 var socket = io();
 
+function replaceAll(orig, toReplace, replaceWith) {
+  var replaced = orig.replace(toReplace, replaceWith);
+  while (replaced.includes(toReplace)) {
+    replaced = replaced.replace(toReplace, replaceWith);
+  }
+  return replaced;
+}
+
 initializeFirebase();
 
 var gameTitleOut = document.getElementById('gameTitleOut');
@@ -7,6 +15,7 @@ var gameTitleIn = document.getElementById('gameTitleIn');
 var descriptionIn = document.getElementById('description');
 var keywordDisplay = document.getElementById('keywordDisplay');
 var keywordIn = document.getElementById('keywordIn');
+var keywordEnter = document.getElementById('enterKeyword');
 var thumbnailImg = document.getElementById('displayThumbnail');
 var thumbnailIn = document.getElementById('thumbnailIn');
 var gameFileUpload = document.getElementById('inputGameFile');
@@ -17,4 +26,21 @@ gameTitleIn.oninput = function() {
   } else {
     gameTitleOut.innerHTML = 'Game Title';
   }
+}
+
+descriptionIn.oninput = function() {
+  if (descriptionIn.value.length > 0) {
+    document.getElementById('descriptionOut').innerHTML = replaceAll(descriptionIn.value, '\n', '<br>');
+  } else {
+    document.getElementById('descriptionOut').innerHTML = 'Game Description';
+  }
+}
+
+keywordEnter.onclick = function(e) {
+  e.preventDefault();
+  var p = document.createElement('p');
+  p.innerHTML = keywordIn.value;
+  p.classList.add('keyword');
+  keywordDisplay.appendChild(p);
+  keywordIn.value = '';
 }
