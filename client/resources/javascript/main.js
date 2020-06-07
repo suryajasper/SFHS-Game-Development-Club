5025b9f42400c7733a774b7b2f549af922718320
Surya Jasper <suryajasper2004@gmail.com>
2020-06-06 22:59:25 -0700

    added way to view only user's games

--- client/resources/javascript/main.js
+++ client/resources/javascript/main.js
@@ -23,51 +23,73 @@ function toggleIMGSize() {
   }
 }
 
-socket.emit('getPublishedGames');
-socket.on('publishedGamesRes', function(res) {
-  for (var gameName of Object.keys(res)) (function(gameName) {
-    var game = res[gameName];
+if ((window.location.href.split('?').length > 1)) {
+  var myGames = document.getElementById('myGames');
+  myGames.innerHTML = 'Go Back';
+  myGames.href = 'main.html';
+}
 
-    var div = document.createElement('div');
-    if ('backgroundColor' in game) {
-      div.style.background = game.backgroundColor;
-    }
-    div.classList.add('game');
+firebase.auth().onAuthStateChanged(function(user) {
+  socket.emit('getPublishedGames');
+  socket.on('publishedGamesRes', function(res) {
+    socket.emit('getUserName', user.uid);
+    socket.on('userNameRes', function(userRes) {
+      var userName = userRes.firstName + ' ' + userRes.lastName;
+      for (var gameName of Object.keys(res)) (function(gameName) {
+        var game = res[gameName];
 
-    var img = document.createElement('img');
-    img.src = game.thumbnail;
-    img.style.cursor = 'pointer';
-    img.onclick = function() {
-      window.location.href = '/game.html?' + gameName;
-    }
-    div.appendChild(img);
+        var div = document.createElement('div');
+        if ('backgroundColor' in game) {
+          div.style.background = game.backgroundColor;
+        }
+        div.classList.add('game');
 
-    var title = document.createElement('h2');
-    title.innerHTML = gameName;
-    if ('fontColor' in game) {
-      title.style.color = game.fontColor;
-    }
-    div.appendChild(title);
+        var img = document.createElement('img');
+        img.src = game.thumbnail;
+        img.style.cursor = 'pointer';
+        img.onclick = function() {
+          window.location.href = '/game.html?' + gameName;
+        }
+        div.appendChild(img);
 
-    var devMentions = document.createElement('p');
-    if ('fontColor' in game) {
-      devMentions.style.color = game.fontColor;
-    }
-    devMentions.classList.add('developers');
-    var developers = 'by ' + game.developers[0];
-    for (var i = 1; i < game.developers.length; i++) {
-      if (i == game.developers.length-1) {
-        developers += ', and ' + game.developers[i];
-      } else {
-        developers += ', ' + game.developers[i];
-      }
-    }
-    devMentions.innerHTML = developers;
-    div.appendChild(devMentions);
+        var title = document.createElement('h2');
+        title.innerHTML = gameName;
+        if ('fontColor' in game) {
+          title.style.color = game.fontColor;
+        }
+        div.appendChild(title);
 
-    document.getElementById('gamesDiv').appendChild(div);
-  })(gameName)
-  toggleIMGSize();
+        var devMentions = document.createElement('p');
+        if ('fontColor' in game) {
+          devMentions.style.color = game.fontColor;
+        }
+        devMentions.classList.add('developers');
+        var developers = 'by ' + game.developers[0];
+        for (var i = 1; i < game.developers.length; i++) {
+          if (i == game.developers.length-1) {
+            developers += ', and ' + game.developers[i];
+          } else {
+            developers += ', ' + game.developers[i];
+          }
+        }
+        devMentions.innerHTML = developers;
+        div.appendChild(devMentions);
+
+        if ((window.location.href.split('?').length > 1)) {
+          if (developers.includes(userName)) {
+            var editButton = document.createElement('button');
+            editButton.innerHTML = 'Edit';
+            editButton.style.textAlign = 'center';
+            div.appendChild(editButton);
+            document.getElementById('gamesDiv').appendChild(div);
+          }
+        } else {
+          document.getElementById('gamesDiv').appendChild(div);
+        }
+      })(gameName)
+      toggleIMGSize();
+    })
+  })
 })
 
 var slider = document.getElementById('imgSizeIn');
