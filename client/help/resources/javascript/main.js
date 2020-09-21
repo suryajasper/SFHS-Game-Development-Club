5d41f7fb164bb94ea30e4bb1a6cf64a1569c1886
Surya Jasper <suryajasper2004@gmail.com>
2020-09-21 13:36:16 -0700

    added way to choose how to sort questions

--- client/help/resources/javascript/main.js
+++ client/help/resources/javascript/main.js
@@ -2,6 +2,8 @@ initializeFirebase();
 
 var socket = io();
 
+var sortType = 'byTopic';
+
 document.getElementById('askQuestion').onclick = function() {
     document.getElementById('askQuestionPopup').style.display = 'block';
 }
@@ -13,51 +15,53 @@ document.getElementById('cancelAskQuestion').onclick = function() {
 document.getElementById('refresh').onclick = function() {
     document.getElementById('refreshGraphic').classList.add('rotate');
     $('#questionPanel').empty();
-    socket.emit('getQuestions');
+    addQuestions(sortType);
 }
 
-function addQuestions(res, sort) {
-    var byDate = {};
-    var byTopic = {};
-    if (res !== null) {
-        $('#questionPanel').empty();
-        for (var topic of Object.keys(res)) {
-            byTopic[topic] = [];
-            for (var date of Object.keys(res[topic])) {
-                var question = res[topic][date];
-                var div = document.createElement('div');
-                div.classList.add('questionBlock');
-                div.innerHTML = '<div class = "questionBlockInner"><p class = "questionBlockQuestion">' + question.question + '</p><p class = "questionBlockTopic">' + question.topic.replaceAll('__sharp__', '#') + '</p><p class = "questionBlockDescription">' + question.description + '</p></div>';
+function addQuestions(sort) {
+    socket.emit('getQuestions');
+    socket.on('questionsRes', function(res) {
+        var byDate = {};
+        var byTopic = {};
+        if (res !== null) {
+            $('#questionPanel').empty();
+            for (var topic of Object.keys(res)) {
+                byTopic[topic] = [];
+                for (var date of Object.keys(res[topic])) {
+                    var question = res[topic][date];
+                    var div = document.createElement('div');
+                    div.classList.add('questionBlock');
+                    div.innerHTML = '<div class = "questionBlockInner"><p class = "questionBlockQuestion">' + question.question + '</p><p class = "questionBlockTopic">' + question.topic.replaceAll('__sharp__', '#') + '</p><p class = "questionBlockDescription">' + question.description + '</p></div>';
 
-                if (date in byDate) byDate[date].push(div);
-                else byDate[date] = [div];
+                    if (date in byDate) byDate[date].push(div);
+                    else byDate[date] = [div];
 
-                byTopic[topic].push(div);
+                    byTopic[topic].push(div);
+                }
             }
         }
-    }
-    var dict;
-    var reverse = false;
-    if (sort === 'byDate' || sort === undefined || sort === null) {
-        dict = byDate;
-        reverse = true;
-    } else if (sort === 'byTopic') {
-        dict = byDate;
-    }
-    var keys = Object.keys(dict);
-    keys.sort();
-    if (reverse)
-        keys.reverse();
-    for (var key of keys) {
-        for (var div of Object.values(dict[key])) {
-            document.getElementById('questionPanel').appendChild(div);
+        var dict;
+        var reverse = false;
+        if (sort === 'byDate') {
+            dict = byDate;
+            reverse = true;
+        } else if (sort === 'byTopic' || sort === undefined || sort === null) {
+            dict = byDate;
         }
-    }
-    document.getElementById('refreshGraphic').classList.remove('rotate');
+        var keys = Object.keys(dict);
+        keys.sort();
+        if (reverse)
+            keys.reverse();
+        for (var key of keys) {
+            for (var div of Object.values(dict[key])) {
+                document.getElementById('questionPanel').appendChild(div);
+            }
+        }
+        document.getElementById('refreshGraphic').classList.remove('rotate');
+    })
 }
 
-socket.emit('getQuestions');
-socket.on('questionsRes', addQuestions);
+addQuestions(sortType);
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -73,3 +77,16 @@ firebase.auth().onAuthStateChanged(function(user) {
         }
     }
 })
+
+var sortChildren = document.getElementById('sortPanel').children;
+for (var selectButton of sortChildren) {
+    selectButton.onclick = function(e) {
+        e.preventDefault();
+        for (var s of sortChildren) {
+            s.classList.remove('buttonSelectSelected');
+        }
+        this.classList.add('buttonSelectSelected');
+        sortType = 'by' + this.textContent;
+        addQuestions(sortType);
+    }
+}
