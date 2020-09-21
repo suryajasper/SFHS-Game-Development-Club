1d44222bc1f2a723707cba293ad0e579d30e0830
Surya Jasper <suryajasper2004@gmail.com>
2020-09-20 20:27:24 -0700

    added popup to ask questions

---
+++ client/help/resources/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('askQuestion').onclick = function() {
+    document.getElementById('askQuestionPopup').style.display = 'block';
+}
+
+document.getElementById('cancelAskQuestion').onclick = function() {
+    document.getElementById('askQuestionPopup').style.display = 'none';
+}
